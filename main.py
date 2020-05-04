f45740799769c02d898d9c6aa0c96880d5c54c57
Riyad <59559190+xLEADERx@users.noreply.github.com>
2020-05-04 18:29:13 +0530

    Update main.py
    Cleaned Code and Added Error handlers 
    Etc..

--- main.py
+++ main.py
@@ -1,44 +1,53 @@
 from selenium import webdriver
-import time
 from selenium.webdriver.common.keys import Keys
+import time
 import re
 import subprocess
+import sys
 import pyautogui
 
-CHAT_NAME = 'Fazil' # GROUP Name
+CHAT_NAME = '' # Group Name
 
-TARGET_LIST = ['Me'] # Add zoom sender name
+TARGET_LIST = [''] # Add zoom sender name
 
-ZOOM_FULL_PATH = r'C:\Users\Arshad\AppData\Roaming\Zoom\bin\Zoom.exe'
+ZOOM_FULL_PATH = r'' #Add Zoom.exe path
 
-WAIT_FOR_DOM_TO_LOAD = 4 # Secs to wait for DOM to load content
+WAIT_FOR_DOM_TO_LOAD = 15 # Secs to wait for DOM to load content
 
 DAY = 'TODAY' # Fetch data from YESTERDAY or TODAY
 
 options = webdriver.ChromeOptions()
 options.add_argument(r"user-data-dir=D:\Python\Memory\WebWhatsAppBot")
 
-navegador = webdriver.Chrome(chrome_options=options)
+navegador = webdriver.Chrome(options=options) # Can Add Drivers Path executable_path="path/to/diver"
 
-navegador.get("https://web.whatsapp.com/")
+def Start():
+    """
+    Search the side panel and click the Chat Name if success return True
+    """
+    navegador.get("https://web.whatsapp.com/")
 
-time.sleep(WAIT_FOR_DOM_TO_LOAD) 
+    time.sleep(WAIT_FOR_DOM_TO_LOAD) 
 
-container = navegador.find_elements_by_xpath('/html/body/div[1]/div/div/div[3]/div/div[2]/div[1]/div/div')# panel-side 
+    container = navegador.find_elements_by_xpath('/html/body/div[1]/div/div/div[3]/div/div[2]/div[1]/div/div')# panel-side 
 
-for item in container:
-    name1 = item.find_element_by_xpath('//*[@id="pane-side"]/div[1]/div/div/div[1]')
-    name2 = item.find_element_by_xpath('//*[@id="pane-side"]/div[1]/div/div/div[12]')
-    name3 = item.find_element_by_xpath('//*[@id="pane-side"]/div[1]/div/div/div[11]')
-    name4 = item.find_element_by_xpath('//*[@id="pane-side"]/div[1]/div/div/div[10]')
+    try:
+        for item in container:
+            name1 = item.find_element_by_xpath('//*[@id="pane-side"]/div[1]/div/div/div[1]')
+            name2 = item.find_element_by_xpath('//*[@id="pane-side"]/div[1]/div/div/div[12]')
+            name3 = item.find_element_by_xpath('//*[@id="pane-side"]/div[1]/div/div/div[11]')
+            name4 = item.find_element_by_xpath('//*[@id="pane-side"]/div[1]/div/div/div[10]')
 
-
-def performClick(*args):
-    for arg in args:
-        if CHAT_NAME in arg.text:
-            arg.click()
-            return 'OK'
-    return None
+        for arg in [name1 , name2 , name3 , name4]:
+            if CHAT_NAME in arg.text:
+                arg.click()
+                return True
+        sys.exit(0)
+    except SystemExit:
+        navegador.close()
+        sys.exit("Unable To Find CHAT_NAME in List")
+    except Exception: 
+        return False
 
 
 def get_id(text):
@@ -48,11 +57,11 @@ def get_id(text):
     NOTE: id must be six digits to be selected edit regex if you need to change it
     """
     try:
-        regex = '[0-9][0-9][0-9] [0-9][0-9][0-9] [0-9][0-9][0-9][0-9]'
+        regex = '[0-9][0-9][0-9] [0-9][0-9][0-9][0-9]? [0-9][0-9][0-9][0-9]'
         text = re.findall(regex, text)
         return text[0]
     except Exception as e:
-        print(e)
+        print('get_id func ' + str(e))
         return None
 
 
@@ -69,7 +78,7 @@ def get_pass(text):
         new_text = new_text[new_text.index(' ') + 1:]
         return new_text
     except Exception as e:
-        print(e)
+        print('get_pass func ' + str(e))
         return None
 
 
@@ -113,39 +122,55 @@ def extractor(raw_msg):
     """
     extract metting id, password and open zoom from DAY specified 
     """
-    if DAY == 'TODAY':
-        raw_msg = raw_msg[str(raw_msg).index(DAY) + 5:] 
-    else:
-        raw_msg = raw_msg[str(raw_msg).index(DAY) + 10:]
+    msg_check = False
+
+    regex = DAY + r'\b'
+    
+    cords = re.search(regex, raw_msg).span(0)
+
+    raw_msg = raw_msg[cords[0] + len(DAY) + 1:]
+
 
     raw_msg = re.split('[0-9][0-9]:[0-9][0-9]\n', raw_msg)
+
     for msg in raw_msg:
         for TARGET in TARGET_LIST:
-            if TARGET in msg and 'Meeting ID:' in msg and 'Password:' in msg:
+            regex = TARGET + r'\b'
+
+            if bool(re.search(regex, msg)) and 'Meeting ID:' in msg and 'Password:' in msg:
                 print(msg)
                 meeting_id = get_id(msg)
                 password = get_pass(msg)
+                msg_check = True
                 open_app(meeting_id, password)
-            elif TARGET in msg and 'Meeting ID:' in msg:
+            elif bool(re.search(regex, msg)) and 'Meeting ID:' in msg:
                 print(msg)
                 meeting_id = get_id(msg)
+                msg_check = True
                 open_app(meeting_id)
+
+    if not msg_check:
+        print('Unable to Find Zoom Message.')
+
     return raw_msg
 
+#main block
+if __name__ == '__main__':
 
-try:
-    if performClick(name1, name2, name3, name4) != None:
-        # Scrape messages inside the sender section
-        messages = navegador.find_elements_by_xpath('/html/body/div[1]/div/div/div[4]/div/div[3]/div/div/div[3]') # Path for messages section
-        for _ in range(8):
-            time.sleep(1)
-            messages[0].send_keys(Keys.PAGE_UP)
+    while True:
+        if Start():
+            break
+        print('Error could not read the DOM content Retrying in 3 Secs...')
+        time.sleep(3)
 
-        for msg in messages:
-            raw_msg = msg.text
-
-        parsed_msg = extractor(raw_msg)
-            
-except Exception as e:
-    print('DOM NOT LOADED TRY INCREASING TIME TO WAIT FOR DOM TO LOAD')
-    print(e)
+    messages = navegador.find_elements_by_xpath('/html/body/div[1]/div/div/div[4]/div/div[3]/div/div/div[3]') # Path for messages section Scrape messages inside the sender section
+      
+    for _ in range(5):
+        time.sleep(1)
+        messages[0].send_keys(Keys.PAGE_UP)
+      
+    for msg in messages:
+        raw_msg = msg.text
+        #print(raw_msg)
+        # sys.exit(0)
+        parsed_msg = extractor(raw_msg)          
